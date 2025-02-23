793385d1377f510853a3b609f95b5ac1c2e74ee5
raj <razbc12@gmail.com>
2025-02-23 19:54:08 +0545

    Complete api fetch logic, html layout

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 "use strict";
 import "./styles.css";
 
+let userLocation = {};
+
 const getPosition = new Promise((resolve, reject) => {
   navigator.geolocation.getCurrentPosition(
     (pos) => resolve(pos),
@@ -15,8 +17,21 @@ getPosition
 
 async function fetchCity(position) {
   let response = await fetch(
-    `https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json
-`
+    `https://nominatim.openstreetmap.org/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&format=json`
   );
   let data = await response.json();
+  userLocation = {
+    city: data.address.county,
+    country: data.address.country,
+  };
+  getWeatherInfo(userLocation.city);
+}
+
+async function getWeatherInfo(city) {
+  let response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=ZSSFTJNCCFT6YGSSHS6WJEN4U&contentType=json`,
+    { mode: "cors" }
+  );
+  let data = await response.json();
+  console.log(data);
 }
